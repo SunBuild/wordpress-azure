462fc423b03a00cc0e34b112f24bd6b2df5e5997
sunitha muthukrishna <sumuth@microsoft.com>
2017-06-14 11:20:42 -0700

    fixed php error in wpconfig.php file

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,17 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('DATABASE_NAME'););
+
+define('DB_NAME', getenv('DATABASE_NAME'));
 
 /** MySQL database username */
-define('DB_USER', getenv('DATABASE_USERNAME'););
+define('DB_USER', getenv('DATABASE_USERNAME'));
 
 /** MySQL database password */
-define('DB_PASSWORD',getenv('DATABASE_PASSWORD'););
+define('DB_PASSWORD',getenv('DATABASE_PASSWORD'));
 
 /** MySQL hostname */
-define('DB_HOST', getenv('DATABASE_HOST'););
+define('DB_HOST', getenv('DATABASE_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
